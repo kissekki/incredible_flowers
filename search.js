3a7c2a4f9bd7c10d12921f23a222fae79fa6b282
kissekki <104277975+kissekki@users.noreply.github.com>
2024-12-01 18:39:56 +0300

    Add files via upload

--- search.js
+++ search.js
@@ -49,7 +49,6 @@ async function getResponce() {
     content_filter= content_price.filter((product) =>{
         return (
                     product.title.toLowerCase().includes(word) ||
-                    product.description.toLowerCase().includes(word) ||
                     product.price.toString().includes(word)
                 );
 
@@ -60,14 +59,14 @@ async function getResponce() {
     //node_for_insert.innerHTML='';
     for (key in content_filter) {
                 node_for_insert.innerHTML += `
-                <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
-                <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
-                <h5 class="card-title">${content_filter[key].title}</h5>
-                <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
-                <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
-                <p class="card-text" >!!Заказать!! <input class="w-25" type="text" value="0" name="check"></p>
-                </li>
-                        `
+                <div class="flower-card">
+                    <li style="width: 400px" class="d-flex flex-column m-1 p-1 border bg-body">
+                    <img style="width: 260px" class="align-self-center" src=${content_filter[key].img}>
+                    <h3 class="flower-caption">${content_filter[key].title}</h3>
+                    <p class="flower-card">Цена ${content_filter[key].price} р.</p>
+                    <p class="flower-caption" >Добавить <input class="w-25" type="text" value="0" name="check"></p>
+                    </li>
+                </div>        `
             }
 
 }
@@ -114,7 +113,6 @@ async function getResponce1() {
     content_filter= content_title.filter((product) =>{
         return (
                     product.title.toLowerCase().includes(word) ||
-                    product.description.toLowerCase().includes(word) ||
                     product.price.toString().includes(word)
                 );
 
@@ -125,13 +123,14 @@ async function getResponce1() {
     let node_for_insert = document.getElementById("node_for_insert")
     for (key in content_filter) {
                 node_for_insert.innerHTML += `
-                <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
-                <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
-                <h5 class="card-title">${content_filter[key].title}</h5>
-                <p class="card-text"> Цена ${content_filter[key].price} р.</p>
-                <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
-                </li>
-                        `
+                <div class="flower-card">
+                    <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
+                    <img style="width: 260px" class="align-self-center" src=${content_filter[key].img}>
+                    <h3 class="flower-caption">${content_filter[key].title}</h3>
+                    <p class="flower-card">Цена ${content_filter[key].price} р.</p>
+                    <p class="flower-caption" >Добавить <input class="w-25" type="text" value="0" name="check"></p>
+                    </li>
+                </div>        `
             }
 
 }
